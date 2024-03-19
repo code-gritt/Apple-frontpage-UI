f8c916f38ea3c12756e7b48e273377d4ab9a33e6
Gokul <gokulchandan24@gmail.com>
2024-03-19 18:27:59 +0530

    started created model component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Model from "./components/Model";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -6,6 +9,9 @@ function App() {
     <>
       <main className="bg-black">
         <Navbar />
+        <Hero />
+        <Highlights />
+        <Model />
         <Footer />
       </main>
     </>
